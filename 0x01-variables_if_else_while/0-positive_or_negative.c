bc0209f767df28efcdfe7396987f5d7ce60f140d
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-11 17:08:59 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,8 +23,8 @@ int main(void)
 			printf("%d is zero\n", n);
 		}
 		else
-	       	{
-			printf("%d is negative\n", n);
-		}
+	       		{
+				printf("%d is negative\n", n);
+			}
 		return (0);
 }
