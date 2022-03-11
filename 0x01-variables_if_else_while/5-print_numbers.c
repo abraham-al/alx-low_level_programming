a6dfb3b9aae0bccc316c43858f360648ec1a54a0
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-12 01:47:43 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,12 @@
+
 #include <stdio.h>
 /**
- *  main - assign a random number to the variable n each time it is executed
- *  if the number is greater than 0: is positive, if the number is 0: is zero
- *  if the number is less than 0: is negative, followed by a new line
- *  Return: 0
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success
  */
 int main(void)
 {
-int a= 0;
-while (a < 10)
-{
-    /* code */
-    printf("%d", a);
-    a++;
+	printf("0123456789\n");
+	return (0);
 }
-printf("\n");
-return (0);
-} 
