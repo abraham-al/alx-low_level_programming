c0c13edb5999007acafe025dccb5c189a4079791
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-11 09:13:29 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(int 1, quote,int 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+        sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
