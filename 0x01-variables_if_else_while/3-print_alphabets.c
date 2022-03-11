f18d7d67e85c6c231cae22c32f2df422d3d2c6f8
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-12 01:36:14 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-char c = 'a';
-char d = 'A';
+	char c = 'a';
+	char d = 'A';
 
-while (c <= 'z')
-{
-	putchar(c);
-	c++;
-}
-while (d <= 'Z')
-{
-	putchar(d);
-	d++;
-}
-putchar('\n');
-return (0);	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);	
  
 }
