1aa34f2cb721df6b2ea399275e7fb91639ed7f96
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-12 01:54:06 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,48 +1,42 @@
+
 #include <stdio.h>
 /**
- * main - prints all numbers of base 16.
+ * main - print numbers from 00 to 99.
 (*
  * Return: 0 on success
  */
 int main(void)
 {
 	int i = '0';
-    int j = '1';
-    int k = '2';
-    while (i <= '7')
-    {
-        /* code */
-        
-        while (j <= '8')
-        {
-            /* code */
-            while (k <= '9')
-            {
-                /* code */            
-                if (!(i > j || j > k || i == j || i == k || j == k))
-                {
-                    /* code */
-                    putchar(i);
-                    putchar(j);
-                    putchar(k);
-                    if (i == '7' && j == '8' && k == '9')
-                    {
-                        /* code */
-                        putchar('\n');
-                    }
-                    else
-                    {
-                        putchar(44);
-                        putchar(32);
-                    } 
-                }
-                k++;
-            }
-            k='3';
-            j++;
-        }
-        j='1';
-        i++;           
-    }
-    return (0);
+	int j = '0';
+	int k = '0';
+
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+				k++;
+			}
+			k = '0';
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
