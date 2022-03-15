1667101e33f37d5b6d6a7dfd51bb216e9821fec2
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-16 01:27:39 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,34 @@
-#include "main.h"
-/**
- * times_table - prints time table in this format: HH:MM
- * Return: the sum of two numbers.
- */
 void times_table(void)
 {
-	int h, m;
+	int row, column, product, tens, ones;
 
-	for (h = 0; h < 24; h++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (m = 0; m < 60; m++)
+		for (column = 0; column <= 9; column++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
