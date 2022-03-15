f20917a3b1ebc287b03e28b7850472a6a6f84a68
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-15 23:56:18 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the english alphabet from a-z.
+ * print_alphabet_x10- prints the english alphabet from a-z.
  * Return: Nothing.
  */
 void print_alphabet_x10(void)
