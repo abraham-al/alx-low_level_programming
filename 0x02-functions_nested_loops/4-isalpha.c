1c417fea03268296a2d545314a2f742be0f59b6c
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-16 00:06:35 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,11 @@
  * _islower - check the code
  *
  * Return: Always 0.
- * r
- * c
+ * @c: character to test.
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'){
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
 	return (1);
     putchar(65);}
 else
