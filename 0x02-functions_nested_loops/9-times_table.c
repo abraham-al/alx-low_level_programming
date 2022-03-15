b6619b8c34df7f9475f091b379cf2e3070bd4eb1
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-16 01:28:53 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+
 void times_table(void)
 {
 	int row, column, product, tens, ones;
