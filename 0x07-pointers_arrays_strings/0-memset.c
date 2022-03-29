4b4686a6c01f52e3c31ad4f5693fcf97b3a6da65
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-03-29 12:31:02 +0300

    a function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - copy the src to dest
+ * @dest: var 1
+ * @src: var 2
+ * @n: var3
+ * Return: return dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		/* code */
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
