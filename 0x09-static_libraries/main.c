9f477ccff83dc80f33c085650286e04256a079ce
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-04-02 02:02:57 +0300

    static

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * Return: output
+ */
+int main(void)
+{
+_puts("\"At the end of the day,my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
