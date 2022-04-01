b20560d37c3c4f5e3c3b29ed2131ec8e673c688a
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-04-01 22:31:33 +0300

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - return the exefile name
+ * Return:always 0
+ * @argc: argument 1
+ * @argv: argument2
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
