5a6851a9367ed185e758c5e6b6538f3dec4f74e3
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-07-01 09:18:12 +0300

    a function that creates a hash table.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,11 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef HOL_HASH
+#define HOL_HASH
+
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+/****** struct *******/
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -11,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+     char *key;
+     char *value;
+     struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,9 +31,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+     unsigned long int size;
+     hash_node_t **array;
 } hash_table_t;
+
+/****** Hash functions *******/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -36,4 +43,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
 #endif
